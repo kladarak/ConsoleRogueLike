68c4b6e6addcbc90fe1d124d1db69472d1413de0
Scott <kladarak@hotmail.com>
2015-07-11 03:12:33 +0100

    Improved render of dungeon map.

--- ConsoleGame/src/StateMachine/ConcreteStates/MapScreenState.cpp
+++ ConsoleGame/src/StateMachine/ConcreteStates/MapScreenState.cpp
@@ -2,10 +2,61 @@
 
 #include <Input/InputBuffer.h>
 #include <Renderer/RenderTargetWriter.h>
+#include <Renderer/AsciiMesh.h>
 
 #include <Dungeon/DungeonMap.h>
 
 #include "GameData.h"
+
+namespace
+{
+	enum ECorner
+	{
+		TopLeft		= 1 << 0,
+		TopRight	= 1 << 1,
+		BottomLeft	= 1 << 2,
+		BottomRight = 1 << 3
+	};
+
+	const char kCornerPiece[]
+	{
+		' ',
+		gCastUCharToChar(217), // TopLeft
+		gCastUCharToChar(192), // TopRight
+		gCastUCharToChar(193), // TopLeft | TopRight
+
+		gCastUCharToChar(191), // BottomLeft
+		gCastUCharToChar(180), // BottomLeft | TopLeft
+		gCastUCharToChar(197), // BottomLeft | TopRight
+		gCastUCharToChar(197), // BottomLeft | TopLeft | TopRight
+		
+		gCastUCharToChar(218), // BottomRight
+		gCastUCharToChar(197), // BottomRight | TopLeft
+		gCastUCharToChar(195), // BottomRight | TopRight
+		gCastUCharToChar(197), // BottomRight | TopLeft | TopRight
+		
+		gCastUCharToChar(194), // BottomLeft | BottomRight
+		gCastUCharToChar(197), // BottomLeft | BottomRight | TopLeft
+		gCastUCharToChar(197), // BottomLeft | BottomRight | TopRight
+		gCastUCharToChar(197), // BottomLeft | BottomRight | TopRight \ TopLeft
+	};
+
+	//const char kRoomPixels[] = 
+	//{
+	//	gCastUCharToChar(218),	'-',	gCastUCharToChar(191),
+	//	'|',					' ',	'|',
+	//	gCastUCharToChar(192),	'-',	gCastUCharToChar(217),
+	//};
+	
+	const char kRoomPixels[] = 
+	{
+		gCastUCharToChar(218), gCastUCharToChar(194), gCastUCharToChar(191),
+		gCastUCharToChar(195), gCastUCharToChar(197), gCastUCharToChar(180),
+		gCastUCharToChar(192), gCastUCharToChar(193), gCastUCharToChar(217),
+	};
+
+	const AsciiMesh kRoomMesh(kRoomPixels, 3, 3);
+}
 
 MapScreenState::MapScreenState(MessageBroadcaster* inStateMachineMsgBroadcaster, GameData* inGameData)
 	: StateBase(inStateMachineMsgBroadcaster, inGameData)
@@ -22,18 +73,46 @@ void MapScreenState::Update(float inFrameTime, const InputBuffer& inInput)
 
 std::string MapScreenState::GetRenderBuffer() const
 {
-	auto& dungeonMap = mGameData->mDungeonMap;
+	auto&	dungeonMap	= mGameData->mDungeonMap;
+	size_t	colCount	= dungeonMap.GetColCount();
+	size_t	rowCount	= dungeonMap.GetRowCount();
 
-	const int kMargin = 2;
-	RenderTargetWriter renderTarget(dungeonMap.GetColCount() + kMargin*2, dungeonMap.GetRowCount() + kMargin*2);
-
-	dungeonMap.ForEach( [&] (size_t inX, size_t inY, const Entity& inRoom)
+	auto isValidRoom = [&] (size_t inCol, size_t inRow)
 	{
-		if (inRoom.IsValid())
+		bool inRange = inCol < colCount && inRow < rowCount;
+		return inRange && dungeonMap.Get(inCol, inRow).IsValid();
+	};
+
+	RenderTargetWriter renderTarget(dungeonMap.GetColCount()*3, dungeonMap.GetRowCount()*3);
+	
+	for (size_t row = 0; row < dungeonMap.GetRowCount(); ++row)
+	{
+		for (size_t col = 0; col < dungeonMap.GetColCount(); ++col)
 		{
-			renderTarget.Write('X', kMargin + inX, kMargin + inY);
+			int mask = 0;
+			mask |= isValidRoom(col,	row)	? TopLeft		: 0;
+			mask |= isValidRoom(col+1,	row)	? TopRight		: 0;
+			mask |= isValidRoom(col,	row+1)	? BottomLeft	: 0;
+			mask |= isValidRoom(col+1,	row+1)	? BottomRight	: 0;
+
+			char piece = kCornerPiece[mask];
+			int x = col * 2;
+			int y = row * 2;
+			renderTarget.Write(piece, x, y);
 		}
-	} );
+	}
+
+	//dungeonMap.ForEach( [&] (size_t inX, size_t inY, const Entity& inRoom)
+	//{
+	//	if (inRoom.IsValid())
+	//	{
+	//
+	//		// * 2 + 1
+	//		int col = inX * 3;
+	//		int row = inY * 3;
+	//		renderTarget.Write(kRoomMesh, col, row);
+	//	}
+	//} );
 
 	return renderTarget.GetRenderBuffer();
 }
